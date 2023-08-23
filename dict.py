7bf5ca73446b8f7e8dfd5a7281b3a0ca4cc25d9b
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-08-22 23:17:40 -0300

    push

---
+++ dict.py
@@ -0,0 +1,21 @@
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+dados = {'nome': 'João', 'idade': 35}
+p1 = Pessoa(**dados)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# p1.__dict__['outra'] = 'coisa'
+# p1.__dict__['nome'] = 'EITA'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+
+print(p1.nome)
